904265f7982112b5481347dfc2467e21801ae571
Martin Garrix <57713995+Aswin-A-Arun@users.noreply.github.com>
2020-02-22 17:42:27 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,43 @@
+class Form {
+	constructor(){
+	this.input= = createInput("name");
+	this.button = createButton('Play');
+	this.greeting = createElement('h2');
+	this.title = createElement('h2');
+	
+}
+
+hide(){
+	this.greeting.hide();
+	this.button.hide();
+	this.input.hide();
+	this.title.height
+}
+
+display(){
+	
+	this.title.html("Car Racing Game");
+	this.title.position(displayWidth/2-50,0);
+
+	//this.input = createInput("name");
+	//this.button = createButton('Play');
+
+	this.input.position(displayWidth/2-50,displayHeight/2-80);
+	this.button.position(displayWidth/2+30,displayHeight/2);
+
+	this.button.MousePressed(function(){
+		this.input.hide();
+		this.button.hide();
+
+		player.name = this.input.value();
+
+		playerCount+=1;
+		player.index = playerCount;
+		player.update();
+		player.updateCount(playerCount);
+		var greeting = createElement('h3');
+		this.greeting.html("Hello" + player.name);
+		this.greeting.position(dsplayWidth/2-70,displayHeight/4);
+		});
+	}
+}
